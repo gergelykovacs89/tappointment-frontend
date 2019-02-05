5145d027e40d03b02e91013788e3b6d1a77146f2
Gergely Kovacs <gergelyk89@gmail.com>
2019-02-05 16:15:34 +0100

    add login route, sends back user info, auth token

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../../services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private userService: UserService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  private initForm() {
+    const email = '';
+    const password = '';
+
+    this.loginForm = new FormGroup({
+        'email': new FormControl(email, Validators.compose([
+          Validators.email,
+          Validators.required])),
+        'password': new FormControl(password, Validators.compose([]))
+      }
+    );
+  }
+
+  onSubmit() {
+    this.userService.loginUser(this.loginForm.value)
+      .subscribe((res) => {
+          if (res['status'] === 'OK') {
+            alert(res['message']);
+            this.router.navigate(['/profiles']);
+          }
+        },
+        (err) => {
+          if (err.error.status === 'WRONG_PASSWORD') {
+            alert('Wrong e-mail or password');
+            this.loginForm.reset();
+          }
+        });
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {UserModel} from '../shared/models/user.model';
+import {map} from 'rxjs/operators';
 
 
 const REG_API_ENDPOINT = 'http://localhost:3000/api/register';
+const LOGIN_API_ENDPOINT = 'http://localhost:3000/api/login';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +16,20 @@ export class UserService {
 
   registerUser(userData: UserModel) {
     return this.httpClient.post(REG_API_ENDPOINT, userData);
+  }
+
+  loginUser(userData: UserModel) {
+    return this.httpClient.post(LOGIN_API_ENDPOINT, {
+      email: userData.email,
+      password: userData.password
+    })
+      .pipe(map((res) => {
+        if (res) {
+          localStorage.setItem('currentUser', JSON.stringify(res['user'], ['email', 'id']));
+          localStorage.setItem('userToken', JSON.stringify(res['userToken']));
+        }
+        return res;
+      }));
   }
 
 
