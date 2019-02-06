479d3ddc64914aa477aa000f0e315bf039bede08
Gergely Kovacs <gergelyk89@gmail.com>
2019-02-06 15:30:26 +0100

    add category accordion view with menuitems showing details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MenuitemModel} from '../shared/models/menuitem.model';
 import {MenuitemService} from '../services/menuitem.service';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,8 @@ export class HomeComponent implements OnInit {
   menuitems: MenuitemModel[];
   categories: Set<string>;
 
-  constructor(private menuItemService: MenuitemService) { }
+  constructor(private menuItemService: MenuitemService,
+              private authService: AuthService) { }
 
   ngOnInit() {
     this.menuItemService.getMenuItems().subscribe((menuitems) => {
@@ -22,4 +24,7 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  getCategoryItems(category: string): MenuitemModel[] {
+    return this.menuitems.filter(item => item.Category === category);
+  }
 }
--- src/app/home/home/home.module.ts
+++ src/app/home/home/home.module.ts
@@ -5,12 +5,14 @@ import {RegisterComponent} from '../register/register.component';
 import {LoginComponent} from '../login/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   declarations: [HomeComponent, RegisterComponent, LoginComponent]
 })
