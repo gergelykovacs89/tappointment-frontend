32d0d0868560442cca63349221441ca1ac17cf18
Gergely Kovacs <gergelyk89@gmail.com>
2019-02-10 16:37:59 +0100

    add auth guard for checkout, cart button disabled if empty cart

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,12 +4,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from '../home/login/login.component';
 import {RegisterComponent} from '../home/register/register.component';
 import {CheckoutComponent} from '../home/checkout/checkout.component';
+import {AuthGuard} from '../auth/auth.guard';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'register', component: RegisterComponent },
   {path: 'login', component: LoginComponent },
-  {path: 'checkout', component: CheckoutComponent },
+  {path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard]},
   { path: '**', redirectTo: '' }
 ];
 
